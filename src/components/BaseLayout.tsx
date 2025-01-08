fe519ea442724ebc91fc8358fb2db43743a65fb3
takemo-sonic <t.kawasaki@sonicmoov.com>
2025-01-08 18:38:38 +0900

    [update] BaseLayout: simplify Authenticator usage by removing provider wrapper

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,16 +1,14 @@
 'use client';
 
 import '@aws-amplify/ui-react/styles.css';
-import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 type BaseLayoutProps = {
   children: React.ReactNode;
 };
 
 const BaseLayout = ({ children }: BaseLayoutProps) => {
-  return <Authenticator.Provider>{children}</Authenticator.Provider>;
+  return <Authenticator>{children}</Authenticator>;
 };
 
-const BaseLayoutWithAuthenticator = withAuthenticator(BaseLayout);
-
-export { BaseLayoutWithAuthenticator as BaseLayout };
+export { BaseLayout };
